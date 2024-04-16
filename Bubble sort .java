1e13055f655e80dfb5c980e814e0ab776472ec34
KranthiK0 <151396320+KranthiK0@users.noreply.github.com>
2024-04-16 15:05:23 -0400

    Update Bubble sort .java

--- Bubble sort .java
+++ Bubble sort .java
@@ -9,13 +9,20 @@ Welcome to GDB Online.
 import java.util.*;
 public class Main {
     static void bubble_sort(int[] arr, int n) {
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j <= i - 1; j++) {
+        for (int i = n - 2; i >= 0; i--) {
+          bool swap = 0 ; 
+            for (int j = 0; j <= i ; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    swap = 1 ; 
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    
                 }
+              if(swap == 0)
+              {
+                break;
+              }
             }
         }
 
